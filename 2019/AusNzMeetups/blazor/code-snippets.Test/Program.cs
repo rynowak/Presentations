4ef8548e8f3a42c9cac9e1ee21a3e7efd7934241
Ryan Nowak <rynowak@microsoft.com>
2019-10-16 19:55:27 -0700

    Add slides for Blazor deep-dive

---
+++ 2019/AusNzMeetups/blazor/code-snippets.Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using code_snippets.Pages;
+using code_snippets.Shared;
+using Microsoft.AspNetCore.Components.Testing;
+
+namespace code_snippets.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TestHost();
+            var component = host.AddComponent<Counter>();
+
+            component.Find("button").Click();
+        }
+    }
+}
